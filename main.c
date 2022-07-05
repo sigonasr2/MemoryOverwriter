625972ce92aa74c14a99dcbdbe9b2b39c796b16c
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-07-05 13:55:29 +0000

    Convert strings to longs
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <sys/uio.h>
+
+unsigned long strToLong(char*str) {
+    int counter=0;
+    char c;
+    unsigned long val=0;
+    while ((c=str[counter++])!='\0') {
+        val*=10;
+        val+=c-'0';
+    }
+    return val;
+}
 
 int main(int argc,char**argv) {
-    printf("Hello World!");
+    unsigned int pid;
+    unsigned long addr;
+    unsigned long val;
+    if (argc<3) {
+        printf("3 arguments required! Only %d specified.",argc);
+    } else {
+        for (int i=1;i<argc;i++) {
+            printf("\nArg %d: %lu",i+1,strToLong(argv[i]));
+        }
+    }
+    printf("\nHello World!");
 }
--- sig
+++ sig
@@ -2,8 +2,8 @@ export AUTO_UPDATE=true
 
 source utils/define.sh
 
-define PROJECT_NAME "CProjectTemplate"
-define CUSTOM_PARAMS "-lncurses"
+define PROJECT_NAME "memovr"
+define CUSTOM_PARAMS ""
 define LANGUAGE "C"
 
 source utils/main.sh
